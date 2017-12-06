3b8cc75641ae01cd4cf5aa21ab7d68324e6bc899
Kieran Hunt <kieran.hunt92@gmail.com>
2017-12-06 08:03:17 +0200

    Add solution for 6a

---
+++ 2017/6a.cr
@@ -0,0 +1,30 @@
+input = File
+  .read("6.txt")
+  .strip
+  .split("\t")
+  .map { |i| i.to_i }
+
+seen_before = [input]
+
+count = 0
+
+loop do
+  input = input.clone
+
+  max_value = input.max
+  max_index = input.index { |i| i == max_value }
+
+  input[max_index.not_nil!] = 0
+
+  index = max_index
+
+  max_value.times do
+    index = (index.not_nil! + 1) % input.size
+    input[index] += 1
+  end
+  count += 1
+  break if seen_before.map { |i| i == input }.any?
+  seen_before << input
+end
+
+puts count
--- 2017/test_all.cr
+++ 2017/test_all.cr
@@ -16,7 +16,8 @@ HEREDOC
   "4a" => "477",
   "4b" => "167",
   "5a" => "315613",
-  "5b" => "22570529"
+  "5b" => "22570529",
+  "6a" => "14029"
 }.each do |problem, solution|
   actual = `./#{problem}`.strip
   puts <<-HEREDOC
